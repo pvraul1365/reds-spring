0e312a4faac1df5be4da535a1895a993d4ab29b2
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-09-13 10:57:26 +0200

    90. Assigment Solution

--- src/main/java/com/rperezv/city/client/CityClient.java
+++ src/main/java/com/rperezv/city/client/CityClient.java
@@ -4,6 +4,7 @@ import com.rperezv.city.dto.City;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
@@ -34,4 +35,11 @@ public class CityClient {
                 .bodyToMono(City.class);
     }
 
+    public Flux<City> getAll() {
+        return this.webClient
+                .get()
+                .retrieve()
+                .bodyToFlux(City.class);
+    }
+    
 }
--- src/main/java/com/rperezv/city/service/CityService.java
+++ src/main/java/com/rperezv/city/service/CityService.java
@@ -2,12 +2,13 @@ package com.rperezv.city.service;
 
 import com.rperezv.city.client.CityClient;
 import com.rperezv.city.dto.City;
-import java.util.concurrent.TimeUnit;
-import org.redisson.api.RMapCacheReactive;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import org.redisson.api.RMapReactive;
 import org.redisson.api.RedissonReactiveClient;
 import org.redisson.codec.TypedJsonJacksonCodec;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -26,23 +27,24 @@ public class CityService {
     @Autowired
     private CityClient cityClient;
 
-    private RMapCacheReactive<String, City> cityMap;
+    private RMapReactive<String, City> cityMap;
 
     public CityService(RedissonReactiveClient client) {
-        this.cityMap = client.getMapCache("city", new TypedJsonJacksonCodec(String.class, City.class));
+        this.cityMap = client.getMap("city", new TypedJsonJacksonCodec(String.class, City.class));
     }
 
-    // get from cache
-    // if empty - get from db / source
-    // put it in cache
-    // return
     public Mono<City> getCity(final String zipCode) {
-        return this.cityMap.get(zipCode)
-                .switchIfEmpty(
-                        this.cityClient.getCity(zipCode)
-                                .flatMap(city -> this.cityMap.fastPut(zipCode, city, 10, TimeUnit.SECONDS)
-                                        .thenReturn(city))
-                );
+        return cityMap.get(zipCode)
+                .onErrorResume(ex -> this.cityClient.getCity(zipCode));
+    }
+
+    @Scheduled(fixedRate = 10_000) // every 10 seconds
+    public void updateCity() {
+        this.cityClient.getAll()
+                .collectList()
+                .map(list -> list.stream().collect(Collectors.toMap(City::getZip, Function.identity())))
+                .flatMap(this.cityMap::putAll)
+                .subscribe();
     }
 
 }
